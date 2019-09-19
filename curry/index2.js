b7095af30e4edd32646ce52cbc9945728ff94ffc
廖颖涛 <liaoyingtao@cvte.com>
2019-09-19 10:10:12 +0800

    curry

--- curry/index2.js
+++ curry/index2.js
@@ -1,8 +1,6 @@
-function sub_curry(fn) {
-  const args = Array.from(arguments).slice(1);
-
-  return function () {
-    return fn.apply(this, args.concat(Array.from(arguments)));
+function sub_curry(fn, args) {
+  return function (...subArgs) {
+    return fn.apply(this, args.concat(subArgs));
   };
 }
 
@@ -12,8 +10,7 @@ function curry(fn, length) {
 
   return function (...args) {
     if (args.length < length) {
-      var combined = [].concat(fn, args);
-      return curry(sub_curry.apply(this, combined), length - args.length);
+      return curry(sub_curry.call(this, fn, args), length - args.length);
     } else {
       return fn(...args);
     }
